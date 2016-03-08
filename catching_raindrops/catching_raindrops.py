79f302594f57e37d0b9beb0f4aca563197d52d34
David Whale <david@thinkingbinaries.com>
2016-03-08 15:12:49 +0000

    Top level sketch of program written

---
+++ catching_raindrops/catching_raindrops.py
@@ -0,0 +1,29 @@
+# catching_raindrops.py  08/03/2016  D.J.Whale
+
+# game parameters
+
+CUP_CAPACITY = 5
+SPEED = 6
+MAX_MISSES = 3
+AUTO_EMPTY = False
+MOVEMENT = True
+SENSITIVITY = 400
+
+high_score = 0
+
+def run():
+    while True:
+        cup_x = 2
+        # show splash screen
+        # if button A
+            # play game
+            # if new high score
+                # record new high score
+                # show high score
+        # else if button B
+            # test movement
+
+# run()
+
+    
+    
