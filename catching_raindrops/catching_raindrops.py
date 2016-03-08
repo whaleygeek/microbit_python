add8bd3940f6185cc8ba8ca645ed8657838d033f
David Whale <david@thinkingbinaries.com>
2016-03-08 15:24:46 +0000

    Framework of game state machine added

--- catching_raindrops/catching_raindrops.py
+++ catching_raindrops/catching_raindrops.py
@@ -6,20 +6,63 @@ CUP_CAPACITY = 5
 SPEED = 6
 MAX_MISSES = 3
 AUTO_EMPTY = False
-MOVEMENT = True
 SENSITIVITY = 400
 
+def get_cup_position():
+    acc = accelerometer.get_x()/SENSITIVITY
+    return math.clamp(0, 4, acc+2)
+    
 def show_splash_screen():
-    pass
+    pass # TODO show an animation until any button pressed
     
 def play_game():
-    pass
+    score = 0
+    drops_in_cup = 0
+    misses = 0
+    drop_x = 0
+    drop_y = 0
+    cup_x = 2
+    state = "NEWDROP"
+    ##start_game
+    while True:
+        if state == "NEWDROP":
+            pass
+            
+        elif state == "RAINING":
+            pass
+            
+        elif state == "ATCUP":
+            pass
+            
+        elif state == "MISS":
+            pass
+            
+        elif state == "CATCH":
+            pass
+            
+        elif state == "FULL":
+            pass
+            
+        elif state == "EMPTYING"
+            pass
+            
+        elif state == "OVERFLOW":
+            pass
+            
+        elif state == "GAMEOVER":
+            pass
+        
 
 def test_movement():
-    pass
+    button_b.reset_presses()
+    while not button_b.was_pressed():
+        x = get_cup_position()
+        display.clear()
+        display.set_pixel(x, 2)
+        sleep(200)
     
 def show_number(n):
-    pass
+    pass # TODO flash digit 4 times then leave it solid
        
 def run():
     high_score = 0
