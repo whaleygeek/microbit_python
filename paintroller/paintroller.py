bee3624d4fdf9a2f4b87d5b697d156da405be72f
David Whale <david@thinkingbinaries.com>
2016-03-12 07:39:08 +0000

    Framework of paintroller game added

---
+++ paintroller/paintroller.py
@@ -0,0 +1,25 @@
+# paintroller.py  12/03/2016  D.J.Whale
+
+# Game parameters
+MAX_GAME_TIME = 60000
+
+def splash_screen():
+    pass
+    
+def play_game():
+    pass
+    
+def flash_digit(d):
+    pass
+    
+def wait_button_b():
+    pass
+    
+def run():
+    while True:
+        splash_screen()
+        score = play_game()
+        flash_digit(score)
+        wait_button_b()
+    
+# run()
